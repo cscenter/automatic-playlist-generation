51645aeb802931ef1cd121dd4e4f3e58fcd0e694
VMAtm <VMAtm@ya.ru>
2016-03-18 01:14:05 +0300

    Classes to methods plus coroutine

--- DataSetCreator/parsers.py
+++ DataSetCreator/parsers.py
@@ -1,55 +1,54 @@
 import eyed3
 
-class AbstractParser:
-    @staticmethod
-    def update(json_data):
-        assert False
+
+def coroutine(gen):
+    @wraps(gen)
+    def inner(*args, **kwargs):
+        g = gen(*args, **kwargs)
+        next(g)
+        return g
+
+    return inner
 
 
-class EchoNestParser:
-    @staticmethod
-    def update(json_data):
-        """
-        Updates the json with all EchoNest data available for this song
-        :param json_data: json object to update """
-        pass
+@coroutine
+def echo_nest_update(json_data):
+    """
+    Updates the json with all EchoNest data available for this song
+    :param json_data: json object to update """
+    pass
 
 
-class EssentiaParser:
-    @staticmethod
-    def update(json_data):
-        """
-        Updates the json with all EchoNest data available for this song
-        
-        :param json_data: json object to update """
-        pass
+@coroutine
+def essentia_update(json_data):
+    """
+    Updates the json with all Essentia data available for this song
+    :param json_data: json object to update """
+    pass
 
 
-class LastFMParser:
-    @staticmethod
-    def update(json_data):
-        """
-        Updates the json with all Last.FM data available for this song
-        :param json_data: json object to update """
-        pass
+@coroutine
+def last_fm_update(json_data):
+    """
+    Updates the json with all Last.FM data available for this song
+    :param json_data: json object to update """
+    pass
 
 
-class LyricsParser:
-    @staticmethod
-    def update(json_data):
-        """
-        TODO: update the json_data object with lyrics
-        something like this:
-        json_data[...] = get_lyrics(json_data['id'])
-        :param json_data: json object to update """
-        pass
+@coroutine
+def lyrics_update(json_data):
+    """
+    TODO: update the json_data object with lyrics
+    something like this:
+    json_data[...] = get_lyrics(json_data['id'])
+    :param json_data: json object to update """
+    pass
 
 
-class ID3v2Parser:
-    @staticmethod
-    def update(json_data):
-        """
-        Updates the json with all ID3v2 tags available for this song
-        import eyed3 required
-        :param json_data: json object to update """
-        pass
+@coroutine
+def id3_v2_update(json_data):
+    """
+    Updates the json with all ID3v2 tags available for this song
+    import eyed3 required
+    :param json_data: json object to update """
+    pass
