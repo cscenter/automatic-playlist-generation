9eeb9e48a0b006e0fa86a465b2dfc99baec1adaa
VMAtm <mikhail.veselov@gmail.com>
2016-03-26 19:02:24 +0300

    Other datasets populate

--- DataSetCreator/datasetPopulate.py
+++ DataSetCreator/datasetPopulate.py
@@ -17,7 +17,7 @@ def broadcast(targets):
             break
 
 for dataset_path in [  # 'VkDataset #2', 'VkDataset #3',
-                     'VkDataset #4', 'VkDataset #5']:
+                     '______Music', '______Music_2']:
     all_parsers = broadcast([id3_v2_update(), last_fm_update()])
     #                          , echo_nest_update(), librosa_update()])
     dp = HardDriveProvider(dataset_path)
