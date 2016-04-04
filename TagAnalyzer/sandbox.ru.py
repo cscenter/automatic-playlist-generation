d39c871bbb18fe028b43654622ad2a430b444e9e
VMAtm <VMAtm@ya.ru>
2016-04-04 20:44:49 +0300

    Minor improvements

--- TagAnalyzer/sandbox.ru.py
+++ TagAnalyzer/sandbox.ru.py
@@ -25,40 +25,49 @@ for song_path in dp.get_all():
 
     song_fm = song['lastfm']
     if song_fm:
-        artist_title = song_fm['artist']
         album_title = song_fm['album']
-        song_to_album[song_path] = album_title
-        artist_plays[artist_title] += int(song_fm['trackplaycount'])
+        if album_title:
+            song_to_album[song_path] = album_title
+            album_listeners[album_title] += int(song_fm['albumlisteners'])
+            album_plays[album_title] += int(song_fm['albumplaycount'])
+
+        artist_title = song_fm['artist']
+        if artist_title:
+            artist_plays[artist_title] += int(song_fm['trackplaycount'])
+            artist_tags[artist_title].extend(song_fm['artisttags'])
+            artist_tags[artist_title].extend(song_fm['albumtags'])
+            artist_tags[artist_title].extend(song_fm['artistsimilar'])
+
         track_plays[song_path] += int(song_fm['trackplaycount'])
         track_listeners[song_path] += int(song_fm['tracklisteners'])
-        album_listeners[album_title] += int(song_fm['albumlisteners'])
-        album_plays[album_title] += int(song_fm['albumplaycount'])
-        artist_tags[artist_title].extend(song_fm['artisttags'])
-        artist_tags[artist_title].extend(song_fm['albumtags'])
-        artist_tags[artist_title].extend(song_fm['artistsimilar'])
         song_tags[song_path].extend(song_fm['tracktags'])
         song_tags[song_path].extend(song_fm['tracksimtags'])
         song_tags[song_path].extend(song_fm['tracksimilar'])
 
 
-def show_kmeans(tests):
+def show_kmeans(tests_dict, title='songs'):
     # plt.plot(tests)
     # plt.show()
     # plt.plot([var for (cent, var) in initial])
     # plt.show()
     # use vq() to get as assignment for each obs.
+    tests = list(map(float, tests_dict.values()))
     initial = [cluster.vq.kmeans(tests, i) for i in range(1, 10)]
-    cent, var = initial[2]
+    cent, var = initial[3]
     assignment, cdist = cluster.vq.vq(tests, cent)
     plt.scatter(range(len(tests)), tests, c=assignment)
+    plt.title(title)
     plt.show()
 
-ap = list(map(float, artist_plays.values()))
-show_kmeans(ap)
+# ap = list(map(float, artist_plays.values()))
+show_kmeans(artist_plays, 'artist_plays')
 
-tp = list(map(float, track_plays.values()))
-show_kmeans(tp)
+# tp = list(map(float, track_plays.values()))
+show_kmeans(track_plays, 'track_plays')
 
-trp = list(map(float, track_listeners.values()))
-show_kmeans(trp)
+# trp = list(map(float, track_listeners.values()))
+show_kmeans(track_listeners, 'track_listeners')
 
+show_kmeans(album_listeners, 'album_listeners')
+
+show_kmeans(album_plays, 'album_plays')
