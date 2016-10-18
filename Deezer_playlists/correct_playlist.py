ccdf85aaed5839790420a336b732875305a32a6b
korskovan <korskovan@gmail.com>
2016-10-18 21:22:38 +0300

    less API calls

--- Deezer_playlists/correct_playlist.py
+++ Deezer_playlists/correct_playlist.py
@@ -11,13 +11,81 @@ path_track_1 = 'http://ws.audioscrobbler.com/2.0/?method=track.getcorrection&art
 path_track_2 = '&track='
 path_track_3 = '&api_key=API_KEY&format=json'
 
-path = '/mnt/hdd3/small_new.txt'
+path = '/mnt/hdd3/small.txt'
 
 tmp_json = 'tmp.json'
 
+artist_bad = set()       # не требуют коррекции
+artist_good = set()      # коррекция не поможет
+artist_rename = {}       # словарь перехода к правильному названию
+
+
+def correct_artist(artist):
+    tmp_json = 'tmp_track.json'
+
+    url_artist = path_before_artist + quote(artist) + path_after_artist
+
+    artist_corr = ""
+
+    try:
+
+        urllib.request.urlretrieve(url_artist, tmp_json)
+
+        with open(tmp_json) as corr_json:
+            corr = json.load(corr_json)
+
+        corr_data = corr['corrections']['correction']['artist']
+
+        if 'name' in corr_data:
+            artist_corr = corr_data['name']
+
+        corr_json.close()
+
+
+    except:
+        print("error for:", artist)
+
+        time.sleep(0.25)
+
+    return artist_corr
+
+
+
+
+def correct_title(artist, title):
+    tmp_json = 'tmp_track.json'
+
+    url_name = path_track_1 + quote(artist) + path_track_2 + quote(title) + path_track_3
+
+    title_corr = ""
+
+    try:
+
+        urllib.request.urlretrieve(url_name, tmp_json)
+
+        with open(tmp_json) as corr_json:
+            corr = json.load(corr_json)
+
+            corr_data = corr['corrections']['correction']['track']
+
+            if 'name' in corr_data:
+                title_corr = corr_data['name']
+
+            corr_json.close()
+    except:
+        print("error for:", artist, "-", title)
+
+    time.sleep(0.25)
+
+
+    return title_corr
+
+
+
+
 
 # на входе json вида {"tracks": [{"track_title": "...", "artist_name": "..."}, ...], "playlist_id": ...}
-# коррекция имени исполнителя, потом коррекция назваия композиции с правильным именем исполнителя
+# коррекция имени исполнителя, потом коррекция названия композиции с правильным именем исполнителя
 
 def correct_pl(line):
     line_json = json.loads(line)
@@ -25,10 +93,9 @@ def correct_pl(line):
     track_list = line_json["tracks"]
     l = len(track_list)
 
-    del_list = []
+    del_list = []          # не получается сделать коррекцию, элементы с этим индексом нужно удалить
 
     for i in range(l):
-        is_artist_correct = 0
 
         track = track_list[i]
 
@@ -37,81 +104,62 @@ def correct_pl(line):
 
         track_name = artist + "-" + title
 
-        # correct artist
-        url_name_1 = path_before_artist + quote(artist) + path_after_artist
 
-        is_artist_correct = 0
-
-        try:
-            urllib.request.urlretrieve(url_name_1, tmp_json)
-
-            with open(tmp_json) as corr_json:
-                corr = json.load(corr_json)
-
-            corr_data = corr['corrections']['correction']['artist']
-
-            if 'name' in corr_data:
-                artist = corr_data['name']
-                track["artist_name"] = artist
-                is_artist_correct = 1
-
-            else:
-                print('artist not corrected', artist)
+        if (artist in artist_good):
+            track_corr = correct_title(artist, title)
+            if (track_corr == ""):
                 del_list.append(i)
+            else:
+                track["track_title"] = track_corr
 
-            corr_json.close()
-
-
-        except:
-            print("urllib error for:", artist)
+        elif (artist in artist_bad):
             del_list.append(i)
 
-        time.sleep(0.25)
-
-        # correct track
-
-        if is_artist_correct:
-
-            url_name_2 = path_track_1 + quote(artist) + path_track_2 + quote(title) + path_track_3
-
-            try:
-
-                urllib.request.urlretrieve(url_name_2, tmp_json)
-
-                with open(tmp_json) as corr_json:
-                    corr = json.load(corr_json)
-
-                corr_data = corr['corrections']['correction']['track']
-
-                if 'name' in corr_data:
-                    title = corr_data['name']
-                    track["track_title"] = title
-                    track_name = artist + "-" + title
-
-                else:
-                    print('title not corrected', title)
-                    del_list.append(i)
-
-                corr_json.close()
-
-
-            except:
-                print("urllib error for:", artist, "-", title)
+        elif (artist in artist_rename):
+            artist_corr = artist_rename["artist"]
+            track["artist_name"] = artist_corr
+            track_corr = correct_title(artist, title)
+            if (track_corr == ""):
                 del_list.append(i)
+            else:
+                track["track_title"] = track_corr
 
-            time.sleep(0.25)
+        else:
+            artist_corr = correct_artist(artist)
+
+            if (artist_corr == ""):
+                del_list.append(i)
+                artist_bad.add(artist)
+            else:
+                if (artist == artist_corr):
+                    artist_good.add(artist)
+                else:
+                    artist_good.add(artist_corr)
+                    artist_rename["artist"] = artist_corr
+                    track["artist_name"] = artist_corr
+
+
+            track_corr = correct_title(artist_corr, title)
+            if (track_corr == ""):
+                del_list.append(i)
+            else:
+                track["track_title"] = track_corr
+
 
     for i in sorted(del_list, reverse=True):
         del track_list[i]
 
-    new_line_json = {}
+    new_l = len(track_list)
 
-    new_line_json["tracks"] = track_list
-    new_line_json["playlist_id"] = line_json["playlist_id"]
+    lines_tsv = []
 
-    new_line = json.dumps(new_line_json)
+    for i in range(new_l):
+        track = track_list[i]
+        tmp_str = track["artist_name"] + '\t' + track["track_title"]
+        lines_tsv.append(tmp_str)
 
-    return new_line
+
+    return lines_tsv
 
 
 path_out = '/mnt/hdd3/small_corr.txt'
@@ -120,8 +168,10 @@ f_out = open(path_out, 'w')
 
 with fileinput.input(files = path) as f:
     for line in f:
-        new_line = correct_pl(line)
-        f_out.write(new_line)
-        f_out.write('\n')
+        new_lines = correct_pl(line)
+        l_n = len(new_lines)
+        for i in range(l_n):
+            f_out.write(new_lines[i])
+            f_out.write('\n')
 
-        # print(fileinput.lineno()) номер строчки, можно записывать отдельно, чтобы прерывать обработку
+        # print(fileinput.lineno())
